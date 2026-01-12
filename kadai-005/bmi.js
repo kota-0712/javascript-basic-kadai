dfb7fb02e2da7fdf0dd1e35d14e9d2700d426d31
光太郎 <>
2026-01-12 20:48:39 +0900

    javascript課題作成

---
+++ kadai-005/bmi.js
@@ -0,0 +1,16 @@
+let userName;
+userName = "weight";
+let useNumber;
+useNumber = 68;
+console.log(userName);
+console.log(useNumber);
+
+userName = "height";
+userNumber = 1.7;
+console.log(userName);
+console.log(userNumber);
+
+userName = "formula";
+console.log(userName);
+let bmi = 68 / (1.7 * 1.7);
+console.log(bmi);
