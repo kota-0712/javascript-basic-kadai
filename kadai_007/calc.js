99cb07ce5a850e054eb8c3d86c1e710ec9dd9d24
光太郎 <>
2026-01-13 20:17:18 +0900

    javascript 007 修正

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -2,13 +2,13 @@ let num = 15;
 
 console.log(num);
 
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log("3と5の倍数です");
+}
 if (num % 3 === 0) {
   console.log("３の倍数です");
-}
-if (num % 5 === 0) {
+} else if (num % 5 === 0) {
   console.log("5の倍数です");
-} else if (num % 3 === 0 && num % 5 === 0) {
-  console.log("3と5の倍数です");
 } else {
-  console.log(num);
+  console.log("num");
 }
